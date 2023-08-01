0e6509b7aadd2983b4b4f9f20870b0bdb6de0b9d
Raphael Martins <raphael.m.nascimento6@aluno.senai.br>
2023-08-01 10:48:54 -0300

    atualizado

--- exercicio 2.py
+++ exercicio 2.py
@@ -3,5 +3,5 @@ num = int(input("diga um número"))
 antecessor = num -1
 sucessor = num +1
 
-print("O antecessor é",antecessor,"O sucessor é",sucessor)
 
+print("O antecessor é",antecessor,"O sucessor é",sucessor)
